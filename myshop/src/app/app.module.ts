c577f7e6151d3ca3b9dc1d3bb24bf2e88ce67477
unknown <mkoifoto@gmail.com>
2021-06-01 21:02:24 +0200

    L4

--- myshop/src/app/app.module.ts
+++ myshop/src/app/app.module.ts
@@ -8,8 +8,9 @@ import { RouterModule, Routes } from '@angular/router';
 import { LandingComponent } from './components/landing/landing.component';
 import { ProductsComponent } from './components/products/products.component';
 import { PanelTopComponent } from './components/panel-top/panel-top.component';
-
+import { CartComponent } from './components/cart/cart.components';
 const appRoutes: Routes = [
+  { path: 'cart', component: CartComponent },
   { path: 'products', component: ProductsComponent },
   { path: 'landing', component: LandingComponent },
   { path: '', redirectTo: '/landing', pathMatch: 'full' }
