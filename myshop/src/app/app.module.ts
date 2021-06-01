91c1baf7c2f2618928110aa31140e5f08f042866
unknown <mkoifoto@gmail.com>
2021-06-01 19:52:26 +0200

    L3

--- myshop/src/app/app.module.ts
+++ myshop/src/app/app.module.ts
@@ -1,16 +1,17 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { RouterModule, Routes } from '@angular/router';
 import { LandingComponent } from './components/landing/landing.component';
 import { ProductsComponent } from './components/products/products.component';
+import { PanelTopComponent } from './components/panel-top/panel-top.component';
 
 const appRoutes: Routes = [
-  { path: 'landing', component: LandingComponent },
   { path: 'products', component: ProductsComponent },
+  { path: 'landing', component: LandingComponent },
   { path: '', redirectTo: '/landing', pathMatch: 'full' }
 ];
 
@@ -18,11 +19,13 @@ const appRoutes: Routes = [
   declarations: [
     AppComponent,
     LandingComponent,
-    ProductsComponent
+    ProductsComponent,
+    PanelTopComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
     RouterModule.forRoot(appRoutes),
   ],
   providers: [],
